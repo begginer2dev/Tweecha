293792f274d5fadab21982f94359d787921ab14e
kashyap211 <kashyapkashyap211@gmail.com>
2025-09-08 02:05:33 +0530

    Update settings.py

--- Tweecha/Tweecha/settings.py
+++ Tweecha/Tweecha/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-2j$)3pk0@w42g21lnz5dw@&xc_1aq$ssp+15f#(it36vp35&(k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Tweecha.onrender.com', 'localhost', '127.0.0.1]
 
 
 # Application definition
@@ -76,10 +77,11 @@ WSGI_APPLICATION = 'chaicentre.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+      'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
